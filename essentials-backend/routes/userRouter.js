19c0bef20ce6bc36217999552267688f82ee6986
Helani1818 <nethmarajapakse@gmail.com>
2021-10-13 17:24:59 +0530

    Modified userControl.js, userRouter.js for login.

--- essentials-backend/routes/userRouter.js
+++ essentials-backend/routes/userRouter.js
@@ -5,5 +5,9 @@ router.post('/register', userControl.register)
 
 router.post('/activation', userControl.activateEmail)
 
+router.post('/login', userControl.login)
+
+router.post('/refresh_token', userControl.getAccessToken)
+
 
 module.exports = router
