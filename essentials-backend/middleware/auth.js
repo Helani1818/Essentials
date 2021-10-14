07ee78994f7289550fe05134350c22b5c8d03522
Helani1818 <nethmarajapakse@gmail.com>
2021-10-14 17:47:04 +0530

    Fixed a bug in auth.js

--- essentials-backend/middleware/auth.js
+++ essentials-backend/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = (req, res, next) => {
         if(!token) return res.status(400).json({msg: "Invalid Authentication"})
 
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
-            if(err) return res.status(400).json({msg: err.message})
+            if(err) return res.status(400).json({msg: "Invalid Authentication."})
 
             req.user = user
             next()
