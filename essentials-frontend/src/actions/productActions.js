1f31ef504b2e92ab0ed7d237c0d73645e218813d
Samadhi Wilegoda <samadhi.wilegoda195@gmail.com>
2021-10-15 12:43:07 +0530

    Added Redux to product screen
    Added Redux to product screen and fixed some errors

--- essentials-frontend/src/actions/productActions.js
+++ essentials-frontend/src/actions/productActions.js
@@ -1,5 +1,12 @@
 import Axios from "axios";
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, } from "../constants/productConstants"
+import { 
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_LIST_FAIL, 
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS, 
+} from "../constants/productConstants"
 
 export const listProducts = () => async(dispatch) => {
     dispatch({
@@ -11,4 +18,21 @@ export const listProducts = () => async(dispatch) => {
     } catch(error){
         dispatch({type: PRODUCT_LIST_FAIL, payload: error.message});
     }
+};
+
+export const detailsProduct = (productID) => async(dispatch) =>{
+    dispatch({type: PRODUCT_DETAILS_REQUEST, payload: productID});
+    try{
+        const{data} = await Axios.get(`/api/products/${productID}`);
+        dispatch ({type: PRODUCT_DETAILS_SUCCESS, payload: data});
+    } catch(error){
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL, 
+            payload: 
+                error.response && error.response.data.message
+                  ?error.response.data.message
+                  :error.message,
+            });
+    }
+
 };
