ffd14e5fd83801beddc4112e1a0c08da5670b609
Samadhi Wilegoda <samadhi.wilegoda195@gmail.com>
2021-10-18 21:59:25 +0530

    Fixed some errors
    Fixed errors in productScreen & productActions

--- essentials-frontend/src/actions/productActions.js
+++ essentials-frontend/src/actions/productActions.js
@@ -20,10 +20,10 @@ export const listProducts = () => async(dispatch) => {
     }
 };
 
-export const detailsProduct = (productID) => async(dispatch) =>{
-    dispatch({type: PRODUCT_DETAILS_REQUEST, payload: productID});
+export const detailsProduct = (productId) => async(dispatch) =>{
+    dispatch({type: PRODUCT_DETAILS_REQUEST, payload: productId});
     try{
-        const{data} = await Axios.get(`/api/products/${productID}`);
+        const{data} = await Axios.get(`/api/products/${productId}`);
         dispatch ({type: PRODUCT_DETAILS_SUCCESS, payload: data});
     } catch(error){
         dispatch({
