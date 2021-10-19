4792c2b429c63eb9c05482ef69524bdc6912a359
Samadhi Wilegoda <samadhi.wilegoda195@gmail.com>
2021-10-19 20:19:14 +0530

    Implemented remove from cart action
    Implemented remove from cart action

--- essentials-frontend/src/screens/CartScreen.js
+++ essentials-frontend/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 import MessageBox from '../components/MessageBox';
 
 export default function CartScreen(props) {
@@ -19,6 +19,7 @@ export default function CartScreen(props) {
      }, [dispatch, productId, qty]);
      const removeFromCartHandler = (id) => {
          //delete action
+         dispatch(removeFromCart(id));
      };
      const checkoutHandler = () => {
          props.history.push('/signin?redirect=sipping');
@@ -29,7 +30,7 @@ export default function CartScreen(props) {
           <h1>           Shopping Cart</h1>
           {cartItems.length === 0 ? (
             <MessageBox>
-              Cart is empty. <Link to="/">Go Shopping</Link>
+              Cart is empty. <Link to="/"> Shop now</Link>
             </MessageBox>
           ) : (
             <ul>
