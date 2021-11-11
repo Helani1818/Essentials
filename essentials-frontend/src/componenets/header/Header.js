4d49efe1b1567ad873487321c26e933c773f0b2c
Helani1818 <nethmarajapakse@gmail.com>
2021-11-11 14:06:23 +0530

    Updated the files to create the navigation bar.

---
+++ essentials-frontend/src/componenets/header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react' 
+import {Link} from 'react-router-dom'
+
+function Header() {
+    return (
+        <header className="row">
+            <div>
+                <h1><Link to="/">Essentials</Link></h1>
+            </div>
+
+            <div>
+                <ul className="nav">
+                    <li><Link to="/">Cart</Link></li>
+                    <li><Link to="/login">Sign In</Link></li>
+                </ul>
+            </div>
+            
+            
+        </header>
+
+        
+    )
+}
+
+export default Header
