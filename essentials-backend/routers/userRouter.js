2c7c08f776ad79adc00b1c0fd7e1b01bf98ffe0a
Helani1818 <nethmarajapakse@gmail.com>
2022-03-25 01:32:46 +0530

    Modified files to create the backend of Register.

--- essentials-backend/routers/userRouter.js
+++ essentials-backend/routers/userRouter.js
@@ -19,7 +19,7 @@ userRouter.get(
 userRouter.post(
   '/signin',
   expressAsyncHandler(async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ email: req.body.email});
     if (user) {
       if (bcrypt.compareSync(req.body.password, user.password)) {
         res.send({
@@ -34,6 +34,26 @@ userRouter.post(
       }
     }
     res.status(401).send({ message: 'Invalid email or password' });
+  })
+);
+
+userRouter.post(
+  '/register',
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      //isSeller: user.isSeller,
+      token: generateToken(createdUser),
+    });
   })
 );
 
