29281872f01cf99b5abef27db793645048675726
Jinushki <jinushki@gmail.com>
2022-03-25 18:10:15 +0530

    Shipping screen conditions - Sign in compulsory

--- essentials-frontend/src/screens/ShippingAddressScreen.js
+++ essentials-frontend/src/screens/ShippingAddressScreen.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { saveShippingAddress } from '../actions/cartActions';
 import CheckoutSteps from "../components/CheckoutSteps";
 
 export default function ShippingAddressScreen(props) {
+    const userSignin = useSelector((state) => state.userSignin);
+    const {userInfo} = userSignin;
+    if (!userInfo) {
+        props.history.push('/signin');
+    }
     const[fullName, setFullName] = useState('');
     const[address, setAddress] = useState('');
     const[city, setCity] = useState('');
